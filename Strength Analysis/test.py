4f3e939a1823a3d91fc570cd61db149aa2c8ce2a
James Yang <45018941+98yangjames@users.noreply.github.com>
2022-02-28 20:46:52 -0800

    New Strengths notebook

--- Strength Analysis/test.py
+++ Strength Analysis/test.py
@@ -1,19 +1,39 @@
 import pandas as pd
 
+import math
 
 
 number_rows = 1000
-df1 = pd.read_csv("unigram_freq.csv", nrows=number_rows)
-df2 = pd.read_csv("rockyousubset.csv", nrows=number_rows)
+country_dataset = pd.read_csv("../Datasets/top_200_password_2020_by_country_extended.csv")
+common_words = pd.read_csv("unigram_freq.csv")
+list_of_bad_words = []
 
+for i in common_words.index:
+    if(len(str(common_words['name'][i])) <= 1):
+        list_of_bad_words.append(i)
+
+common_words = common_words.drop(list_of_bad_words)
+
+country_dataset['Data Length'] = country_dataset['Password'].str.len()
+
+L = country_dataset['Data Length']
+N = 127 #number of symbols that can be typed (ascii table)
+
+country_dataset['strength'] = L*(math.log(N))/math.log(2)
+
+country_dataset.sort_values(by=['Data Length'])
+country_dataset.dropna()
+
+# Calculate the list of matches with the most common words
 list_of_matches = []
 
+for i in country_dataset.index:
+    match = ""
+    for j in common_words.index:
+        if (str(common_words['name'][j]) in str(country_dataset['Password'][i])):
+            match += str(common_words['name'][j]) + ","
 
-# for i in file.index:
-#     for j in common_words.index:
-#         if(common_words['name'][j] in file['name'][i]):
-#             list_of_matches.append(common_words['name'][j])
+    print(match)
+    list_of_matches.append(match[:-1])
 
-
-for ind1 in df1.index:
-    df1.loc[ind1, 'df2_match'] = ', '.join(list(df2[df2['name'].str.contains(df1['name'][ind1])]['name']))
+print(list_of_matches)
